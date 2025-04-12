316803ed924ad96bceaf108cc962a40b3bce44b6
lszydlik <01187295@pw.edu.pl>
2025-04-13 01:36:47 +0200

    feat: add example for Liskov Substitution Princple

---
+++ liskov-substitution.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <numbers>
+
+class Shape
+{
+public:
+    virtual double area() const = 0;
+    virtual ~Shape() = default;
+};
+
+class Rectangle : public Shape
+{
+public:
+    Rectangle(double width, double height) : width_(width), height_(height) {};
+
+    double area() const override
+    {
+        return width_ * height_;
+    }
+
+private:
+    double width_, height_;
+};
+
+class Circle : public Shape
+{
+public:
+    Circle(double radius) : radius_(radius) {};
+
+    double area() const override
+    {
+        // -std=c++20
+        return std::numbers::pi * radius_;
+    }
+
+private:
+    double radius_;
+};
+
+void printArea(const Shape &shape)
+{
+    std::cout << "Area: " << shape.area() << std::endl;
+}
+
+int main()
+{
+    Rectangle rect(3.5, 2.0);
+    Circle circle(5.0);
+
+    // Liskov Substitution Principle:
+    // Objects of type Rectangle and Circle, which are subtypes of Shape,
+    // can be passed to the printArea function.
+    printArea(rect);
+    printArea(circle);
+
+    return 0;
+}
